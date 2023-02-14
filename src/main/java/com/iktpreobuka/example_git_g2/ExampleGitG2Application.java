d22d6223912ca8cfc47b6788ffb3f857b1846978
Sasa Stevovic <sasastevovic1993@gmail.com>
2023-02-14 20:43:25 +0100

    moj prvi commit

---
+++ src/main/java/com/iktpreobuka/example_git_g2/ExampleGitG2Application.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.example_git_g2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGitG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGitG2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iktpreobuka/example_git_g2/ExampleGitG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.example_git_g2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGitG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
